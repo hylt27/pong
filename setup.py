cc927d338fc481aee49f3f39ee76af8430c8e257
Henrique Yukio <hylt27@gmail.com>
2023-01-06 23:40:04 -0300

    added the oppponent IA and ball reset func

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import pygame
 import sys
+import random
 import numpy as np
 
 def change_color():
     new_color = list(np.random.randint(0, 256, 3))
     return new_color
+
 
 def ball_animation():
     global ball_x_speed, ball_y_speed, color
@@ -17,12 +19,37 @@ def ball_animation():
         color = change_color()
 
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_x_speed *= -1
+        ball_restart()
         color = change_color()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_x_speed *= -1
         color = change_color()
+
+def ball_restart():
+    global ball_x_speed, ball_y_speed
+    ball.center = (screen_width/2, screen_height/2)
+    ball_x_speed *= random.choice((1, -1))
+    ball_y_speed *= random.choice((1, -1))
+
+def player_animation():
+    global player_speed
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+def opponent_animation():
+    global opponent_speed
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
 
 # general setup
 pygame.init()
@@ -40,17 +67,18 @@ player = pygame.Rect(screen_width - 10, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(0, screen_height/2 - 70, 10, 140)
 
 #colors
-bg_color = pygame.Color('gray12')
-light_grey = [200, 200, 200]
+bg_color = [191, 191, 191]
+black = [0, 0, 0]
 red = [150, 21, 28]
 purple = [154, 55, 240]
 cyan = [35, 186, 216]
 color = cyan
 
 #ball speed
-ball_x_speed = 7
-ball_y_speed = 6
+ball_x_speed = 7 * random.choice((1, -1))
+ball_y_speed = 7 * random.choice((1, 1))
 player_speed = 0
+opponent_speed = 7
 
 while True:
     # handling output
@@ -71,14 +99,16 @@ while True:
 
 
     ball_animation()
-    player.y += player_speed
+    player_animation()
+    opponent_animation()
+
 
     #visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, purple, player)
     pygame.draw.rect(screen, red, opponent)
     pygame.draw.ellipse(screen, color, ball)
-    pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2, screen_height))
+    pygame.draw.aaline(screen, black, (screen_width/2,0), (screen_width/2, screen_height))
     
     #updating the window
     pygame.display.flip()
