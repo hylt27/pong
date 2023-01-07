3ca7aec523c681913e1005a5a5b0782cb1c71bfc
Henrique Yukio <hylt27@gmail.com>
2023-01-07 16:16:42 -0300

    added scoreboard

--- setup.py
+++ setup.py
@@ -7,24 +7,31 @@ def change_color():
     new_color = list(np.random.randint(0, 256, 3))
     return new_color
 
-
 def ball_animation():
-    global ball_x_speed, ball_y_speed, color
+    global ball_y_speed, ball_x_speed, player_score, opponent_score, scoreboard_color, ball_color
 
     ball.x += ball_x_speed
     ball.y += ball_y_speed
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_y_speed *= -1
-        color = change_color()
+        ball_color = change_color()
 
-    if ball.left <= 0 or ball.right >= screen_width:
+    if ball.left <= 0:
+        player_score += 1
+        scoreboard_color = [154, 55, 240]
         ball_restart()
-        color = change_color()
+        ball_color = change_color()
+
+    if ball.right >= screen_width:
+        ball_restart()
+        opponent_score += 1
+        scoreboard_color = [150, 21, 28]
+        ball_color = change_color()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_x_speed *= -1
-        color = change_color()
+        ball_color = change_color()
 
 def ball_restart():
     global ball_x_speed, ball_y_speed
@@ -65,7 +72,7 @@ pygame.display.set_caption('Pong')
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 10, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(0, screen_height/2 - 70, 10, 140)
-scoreboard = pygame.Rect(screen_width/2 - 70, 10, 140, 30)
+scoreboard = pygame.Rect(screen_width/2 - 30, 10, 60, 30)
 
 # colors
 bg_color = [191, 191, 191]
@@ -73,13 +80,20 @@ black = [0, 0, 0]
 red = [150, 21, 28]
 purple = [154, 55, 240]
 cyan = [35, 186, 216]
-color = cyan
+ball_color = cyan
+scoreboard_color = bg_color
 
-# ball speed
+
+# speeds
 ball_x_speed = 7 * random.choice((1, -1))
 ball_y_speed = 7 * random.choice((1, 1))
 player_speed = 0
 opponent_speed = 7
+
+# text variable
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
 
 while True:
     # handling output
@@ -103,14 +117,19 @@ while True:
     player_animation()
     opponent_animation()
 
-
     # visuals
     screen.fill(bg_color)
-    pygame.draw.rect(screen, purple, scoreboard)
+    pygame.draw.rect(screen, scoreboard_color, scoreboard)
     pygame.draw.rect(screen, purple, player)
     pygame.draw.rect(screen, red, opponent)
-    pygame.draw.ellipse(screen, color, ball)
+    pygame.draw.ellipse(screen, ball_color, ball)
     pygame.draw.aaline(screen, black, (screen_width/2, 0), (screen_width/2, screen_height))
+
+    player_text = game_font.render(f'{player_score}', True, black)
+    screen.blit(player_text, (650, 11))
+    opponent_text = game_font.render(f'{opponent_score}', True, black)
+    screen.blit(opponent_text, (613.5, 11))
+
 
     # updating the window
     pygame.display.flip()
